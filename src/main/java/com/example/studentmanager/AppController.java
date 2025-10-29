c2d3cb7440beecc6de5dfde27b5bdfd11c358033
nino <nino.mauuary@gmail.com>
2025-10-29 16:34:41 +0100

    Init project

--- src/main/java/com/example/studentmanager/AppController.java
+++ src/main/java/com/example/studentmanager/AppController.java
@@ -4,16 +4,35 @@ import javafx.beans.binding.Bindings;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.TableView;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Region;
+
+import java.io.IOException;
 
 public class AppController {
+  @FXML private FlowPane studentCardGrid;
+  @FXML private HBox studentCardList;
   @FXML private TableView<Student> TVStudent;
   @FXML private ObservableList<Student> studentData = FXCollections.observableArrayList();
 
   @FXML
   public void initialize() {
-    this.initializeStudentTableView();
+    for (int i = 0; i < 10; i++) {
+      try {
 
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("studentcard-view.fxml"));
+        Region studentCard = fxmlLoader.load();
+
+        studentCardGrid.getChildren().add(studentCard);
+      } catch (IOException e) {
+        e.printStackTrace();
+        throw new RuntimeException(e);
+      }
+    }
   }
 
   @FXML
---
+++ src/main/java/com/example/studentmanager/StudentCardController.java
@@ -0,0 +1,3 @@
+package com.example.studentmanager;
+
+public class StudentCardController {}
